4a737dfa761006ec050b782f45d509faaa9cdd15
Andrey Latyshev <alatyshe@e3r5p1.unit.ua>
2017-09-11 15:14:15 +0300

    update header file

--- alatyshe/assembler/header/op.h
+++ alatyshe/assembler/header/op.h
@@ -3,28 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   op.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alatyshe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/12 16:17:23 by alatyshe          #+#    #+#             */
-/*   Updated: 2016/12/12 16:22:52 by alatyshe         ###   ########.fr       */
+/*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
+/*   Updated: 2013/11/06 14:21:46 by zaz              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Toutes les tailles sont en octets.
+** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
+*/
+
 #ifndef OP_H
 # define OP_H
-# define CHAR_BIT	8
-# define CHAR_MIN	–128
-# define CHAR_MAX	127
-# define UCHAR_MAX	255
-# define SHRT_MIN	–32768
-# define SHRT_MAX	32767
-# define USHRT_MAX	65535
-# define INT_MIN	–2147483648
-# define INT_MAX	2147483647
-# define UINT_MAX	4294967295
-# define _I64_MIN	-9223372036854775808
-# define _I64_MAX	9223372036854775807
-# define _UI64_MAX	18446744073709551615
 
 # define IND_SIZE				2
 # define REG_SIZE				4
@@ -34,28 +26,30 @@
 # define DIR_CODE				2
 # define IND_CODE				3
 
-# define MAX_ARGS_NUMBER		4
-# define MAX_PLAYERS			4
+# define MAX_ARGS_NUMBER			4
+# define MAX_PLAYERS				4
 # define MEM_SIZE				(4*1024)
-# define IDX_MOD				(MEM_SIZE / 8)
+# define IDX_MOD					(MEM_SIZE / 8)
 # define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
 
 # define COMMENT_CHAR			'#'
 # define LABEL_CHAR				':'
-# define DIRECT_CHAR			'%'
+# define DIRECT_CHAR				'%'
 # define SEPARATOR_CHAR			','
 
-# define LABEL_CHARS			"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-# define NAME_CMD_STRING		".name"
+# define NAME_CMD_STRING			".name"
 # define COMMENT_CMD_STRING		".comment"
 
 # define REG_NUMBER				16
 
 # define CYCLE_TO_DIE			1536
-# define CYCLE_DELTA			50
+# define CYCLE_DELTA				50
 # define NBR_LIVE				21
 # define MAX_CHECKS				10
+
+typedef char	t_arg_type;
 
 # define T_REG					1
 # define T_DIR					2
@@ -65,5 +59,52 @@
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
+
+typedef struct		s_op
+{
+	char			*name;
+	int				count_arg;
+	int				arg[MAX_ARGS_NUMBER];
+	int				op_code;
+	int				cycle;
+	char			*full_name;
+	int				coding_byte;
+	int				flag_direct_size;
+}					t_op;
+
+static	t_op		g_tab[17] =
+{
+	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 0},
+	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 0},
+	{"st", 2, {T_REG, T_IND | T_REG}, 3, 5, "store", 1, 0},
+	{"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1, 0},
+	{"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction", 1, 0},
+	{"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 6,
+		"et (and  r1, r2, r3   r1&r2 -> r3", 1, 0},
+	{"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 6,
+		"ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0},
+	{"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6,
+		"ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0},
+	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1},
+	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25,
+		"load index", 1, 1},
+	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25,
+		"store index", 1, 1},
+	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1},
+	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0},
+	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50,
+		"long load index", 1, 1},
+	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},
+	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0},
+	{0, 0, {0}, 0, 0, 0, 0, 0}
+};
+
+typedef struct			s_header
+{
+	unsigned int		magic;
+	char 				prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int 		prog_size;
+	char 				comment[COMMENT_LENGTH + 1];
+}						t_header;
 
 #endif

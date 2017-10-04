cc6e10018739f3693624fed40242fd086240aaa2
alatyshe <la07121610@gmail.com>
2017-10-04 21:50:47 +0300

    add memmory map

--- ex.s
+++
@@ -1,7 +0,0 @@
-.name "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-.comment "HAHAHAaaa"
-
-
-# haha:	live %10
-live:	ld -10,r1
-# add 	r1,r1,r1   
